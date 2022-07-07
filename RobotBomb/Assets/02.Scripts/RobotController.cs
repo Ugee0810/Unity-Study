ded8e1df829196c342bb084002f014f9270ce4cc
HwanUk Song <shu990810@naver.com>
2022-07-07 11:22:01 +0900

    22-07-07 11:21
    2D 예제 - 이동할 때, 멈출 때 자연스러운 애니메이션 전환 구현

---
+++ RobotBomb/Assets/02.Scripts/RobotController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour
+{
+    Rigidbody2D robotRd;
+    Animator animator;
+
+    public float walkSpeed;
+    public float walkSpeedMax;
+    private int moveCount;
+
+    void Start()
+    {
+        robotRd  = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Debug를 통해 오브젝트의 x위치값을 확인하여, 화면에 나가지 않는 좌표 찾기
+        // print("x : " + transform.position.x);
+
+        // Mathf.Abs() - 유니티에서의 Math함수, 절대값 | velocity.x - 가속도 체크
+        // float speedx = Mathf.Abs(robotRd.velocity.x);
+        // print($"robotRd.velocity.x = {speedx}");
+
+        print($"moveCount : {moveCount}");
+
+        moveCount = 0;
+
+        // Robot Move ←
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x >= -8.0f)
+        {
+            moveCount++;
+            animator.SetBool("isWalking", true);
+            transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+            // transform.localScale = new Vector2(x, y) - 좌우 반전(오브젝트의 크기만큼 빼거나 더함)
+            transform.localScale = new Vector2(-2.0f, 2.0f);
+        }
+
+        // Robot Move →
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x <= 8.0f)
+        {
+            moveCount++;
+            animator.SetBool("isWalking", true);
+            transform.Translate(Vector2.right * walkSpeed * Time.deltaTime);
+            transform.localScale = new Vector2(2.0f, 2.0f);
+        }
+
+        // Robot Stop Idle(속도를 이용해 싱크 조절)
+        if (moveCount == 0)
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+}
