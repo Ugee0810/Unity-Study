5c6580705633e4046f38074576f4ef85bd8b2682
HwanUk Song <shu990810@naver.com>
2022-07-12 16:34:50 +0900

    22-07-12 16:34

---
+++ FlappyBird/Assets/02.Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * 200);
+    }
+}
