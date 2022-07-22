ca72c4df32bf71a15462c13afd90bed734ffae69
HwanUk Song <shu990810@naver.com>
2022-07-22 18:02:22 +0900

    220722 18:02

---
+++ UnityTest_2D_Roguelike2/Assets/02.Scripts/BannerAdmob.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class BannerAdmob : MonoBehaviour
+{
+    private BannerView bannerView;
+
+    void Start()
+    {
+        MobileAds.Initialize(initState => { });
+
+        this.RequestBanner();
+    }
+
+    private void RequestBanner()
+    {
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-1553418165038473/2152114256";
+#elif UNITY_IPHONE
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+        if (this.bannerView != null)
+        {
+            bannerView.Destroy();
+        }
+
+        AdSize adaptiveSize = AdSize.GetCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(AdSize.FullWidth);
+
+        this.bannerView = new BannerView(adUnitId, adaptiveSize, AdPosition.Bottom);
+
+        AdRequest request = new AdRequest.Builder().Build();
+
+        this.bannerView.LoadAd(request);
+    }
+}
