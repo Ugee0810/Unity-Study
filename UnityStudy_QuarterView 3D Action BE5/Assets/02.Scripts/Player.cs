9732002da71f2ce2104f03a947feecfe27202442
HwanUk Song <shu990810@naver.com>
2022-07-23 17:04:12 +0900

    220723 17:04

---
+++ UnityStudy_QuarterView 3D Action BE5/Assets/02.Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+
+    float hAxis;
+    float vAxis;
+
+    Vector3 MoveVec;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        MoveVec = new Vector3(hAxis, 0, vAxis).normalized; // normalized - 어떤 방향이든 값이 1로 보정된 벡터(ft.대각선)
+        transform.position += MoveVec * moveSpeed * Time.deltaTime;
+    }
+}
