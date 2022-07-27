2e3c279e834fc65749f00d04812ee1e97b36b7a1
HwanUk Song <shu990810@naver.com>
2022-07-27 18:24:10 +0900

    220727 18:23

---
+++ PhotonEx/Assets/02.Scripts/MatchMaker.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityChan;
+
+public class MatchMaker : MonoBehaviourPunCallbacks
+{
+    public GameObject photonObject;
+
+    void Start()
+    {
+        Debug.Log("Start()");
+
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.GameVersion = "0.1";
+    }
+    
+    // 조인 타입
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster()");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom() was Called by PUN. Now this client is in a room.");
+
+        float randomX = Random.Range(-6f, 6f);
+
+        PhotonNetwork.Instantiate(
+            photonObject.name,
+            new Vector3(randomX, 1f, 0f),
+            Quaternion.identity,
+            0);
+
+        GameObject mainCamera = GameObject.FindWithTag("MainCamera");
+        mainCamera.GetComponent<ThirdPersonCamera>().enabled = true; // using UnityChan
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4 });
+    }
+}
